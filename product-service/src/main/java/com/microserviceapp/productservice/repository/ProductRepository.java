2ae632d083e9b888cb9b847c23f089be6e837092
laze03 <Lazrak.edu@gmail.com>
2024-02-15 03:56:43 +0100

    product service working

---
+++ product-service/src/main/java/com/microserviceapp/productservice/repository/ProductRepository.java
@@ -0,0 +1,8 @@
+package com.microserviceapp.productservice.repository;
+
+import com.microserviceapp.productservice.model.Product;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface ProductRepository extends MongoRepository<Product, String> {
+
+}
