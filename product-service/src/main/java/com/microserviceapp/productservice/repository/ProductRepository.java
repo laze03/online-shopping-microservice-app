2bd10be535673be92c421a6d860050b9e7bd1bfe
laze03 <Lazrak.edu@gmail.com>
2024-02-15 05:14:17 +0100

    change folder

--- product-service/src/main/java/com/microserviceapp/productservice/repository/ProductRepository.java
+++
@@ -1,8 +0,0 @@
-package com.microserviceapp.productservice.repository;
-
-import com.microserviceapp.productservice.model.Product;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface ProductRepository extends MongoRepository<Product, String> {
-
-}
